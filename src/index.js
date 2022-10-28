0643cacc66f918e3bf2c846173af5fae651ba207
sww <980038858@qq.com>
2022-10-28 13:20:17 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 // 引入react
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ConfigProvider } from 'antd'
+import zhCN from 'antd/es/locale/zh_CN'
 import App from './App';
 
 // 全局样式
 import '@/common/stylus/frame.styl'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const antdConfig = {
+    locale: zhCN,
+}
+
+ReactDOM.render(
+    <ConfigProvider {...antdConfig} >
+        <App />
+    </ConfigProvider >,
+    document.getElementById('root')
+)
