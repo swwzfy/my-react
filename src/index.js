04d5786944127f516b4fd0df7d71f310b8d394b0
sww <980038858@qq.com>
2022-10-28 13:06:25 +0800

    引入Ant Design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+// 引入react
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+// 全局样式
+import '@/common/stylus/frame.styl'
 
 ReactDOM.render(<App />, document.getElementById('root'))
