8bd7da86315660e13fe6e8919eb2720a03ce9647
sww <980038858@qq.com>
2022-10-28 14:09:17 +0800

    组件外跳转

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import { Button } from 'antd'
+import { goto } from '@/api'
 import './home.styl'
 
 function Home() {
@@ -9,6 +10,9 @@ function Home() {
     return (
         <div className="P-home">
             <h1>Home Page</h1>
+            <div className="ipt-con">
+                <Button onClick={() => { goto('/login') }}>组件外跳转</Button>
+            </div>
             <div className="ipt-con">
                 <Button onClick={() => { navigate('/login') }}>返回登录</Button>
             </div>
