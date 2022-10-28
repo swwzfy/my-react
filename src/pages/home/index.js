2c14eff83f1d04ea624731e7fa01dee4fac3847e
sww <980038858@qq.com>
2022-10-28 14:01:20 +0800

    添加路由

---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import { Button } from 'antd'
+import './home.styl'
+
+function Home() {
+
+    return (
+        <div className="P-home">
+            <h1>Home Page</h1>
+            <div className="ipt-con">
+                <Button>返回登录</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
